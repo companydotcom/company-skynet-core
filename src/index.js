14a44f4d6ce5a32902e0048aa7523bfa1784282d
Bharath Kishore Aluri <bharathkishore.a@accionlabs.com>
2019-11-29 12:14:52 +0530

    Added/ updated docs for all functions

--- src/index.js
+++ src/index.js
@@ -3,19 +3,54 @@ import { handler as dTH } from './handlers/directTransition';
 import { handler as bTH } from './handlers/bulkTransition';
 import { handler as sDb } from './handlers/setupDatabase';
 
-
+/**
+ * This is the fetch request handler
+ * @param {object} AWS is the AWS sdk instance that needs to be passed from the handler
+ * @param { throttleLmts: object, safeThrottleLimit: int, reserveCapForDirect: int, retryCntForCapacity: int } are the throttle limits 
+ * @param {string} r is the region of AWS that this service is running in
+ * @param {string} s service is the name of the service
+ * @param {string} a account is AWS the account number
+ * @param {object} b is the event input
+ * @param {function} c is the worker function that has the business logic to process the request
+ */
 export const fetchHandler = async (AWS, { throttleLmts, safeThrottleLimit, reserveCapForDirect, retryCntForCapacity }, r, s, a, b, c) => {
   return fH(AWS, { throttleLmts, safeThrottleLimit, reserveCapForDirect, retryCntForCapacity }, r, s, a, b, c);
 };
 
+/**
+ * This is the direct transition request handler
+ * @param {object} AWS is the AWS sdk instance that needs to be passed from the handler
+ * @param { throttleLmts: object, safeThrottleLimit: int, reserveCapForDirect: int, retryCntForCapacity: int } are the throttle limits 
+ * @param {string} r is the region of AWS that this service is running in
+ * @param {string} s service is the name of the service
+ * @param {string} a account is AWS the account number
+ * @param {object} b is the event input
+ * @param {function} c is the worker function that has the business logic to process the request
+ */
 export const directTransitionHandler = async (AWS, { throttleLmts, safeThrottleLimit, reserveCapForDirect, retryCntForCapacity }, r, s, a, b, c) => {
   return dTH(AWS, { throttleLmts, safeThrottleLimit, reserveCapForDirect, retryCntForCapacity }, r, s, a, b, c);
 };
 
+/**
+ * This is the bulk transition request handler
+ * @param {object} AWS is the AWS sdk instance that needs to be passed from the handler
+ * @param { throttleLmts: object, safeThrottleLimit: int, reserveCapForDirect: int, retryCntForCapacity: int } are the throttle limits 
+ * @param {string} r is the region of AWS that this service is running in
+ * @param {string} s service is the name of the service
+ * @param {string} a account is AWS the account number
+ * @param {object} b is the event input
+ * @param {function} c is the worker function that has the business logic to process the request
+ */
 export const bulkTransitionHandler = async (AWS, { throttleLmts, safeThrottleLimit, reserveCapForDirect, retryCntForCapacity }, r, s, a, b, c) => {
   return bTH(AWS, { throttleLmts, safeThrottleLimit, reserveCapForDirect, retryCntForCapacity }, r, s, a, b, c);
 };
 
+/**
+ * This is the fetch request handler
+ * @param {object} AWS is the AWS sdk instance that needs to be passed from the handler
+ * @param {Object} d is the data to be saved
+ * @param {string} s service is the name of the service
+ */
 export const setupDatabase = async (AWS, d, s) => {
   let data = '';
   if (typeof d === 'object') {
