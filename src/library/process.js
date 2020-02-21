7538eb4442bc359f14583fe678bf07df533235a5
Bharath Kishore Aluri <bharathkishore.a@accionlabs.com>
2020-02-21 20:59:36 +0530

    Fixed issue in saving and retrieving serviceAccount data and added the same feature for serviceUserData

--- src/library/process.js
+++ src/library/process.js
@@ -10,6 +10,46 @@ import {
 } from './queue';
 
 /**
+ * Get the current account data from the database for the given accountId
+ * @param {object} AWS is the AWS sdk instance that needs to be passed from the handler
+ * @param {string} accountId is the accountId for which the data needs to be fetched
+ */
+const getCurrentAccountData = async (AWS, accountId) => {
+  const fetchResponse = await fetchRecordsByQuery(
+    AWS,
+    {
+      TableName: 'Account',
+      ExpressionAttributeNames: { '#pk': 'accountId' },
+      KeyConditionExpression: '#pk = :accId',
+      ExpressionAttributeValues: {
+        ':accId': { S: accountId },
+      },
+    },
+  );
+  return fetchResponse[0];
+};
+
+/**
+ * Get the current user data from the database for the given accountId
+ * @param {object} AWS is the AWS sdk instance that needs to be passed from the handler
+ * @param {string} userId is the userId for which the data needs to be fetched
+ */
+const getCurrentUserData = async (AWS, userId) => {
+  const fetchResponse = await fetchRecordsByQuery(
+    AWS,
+    {
+      TableName: 'User',
+      ExpressionAttributeNames: { '#pk': 'userId' },
+      KeyConditionExpression: '#pk = :uId',
+      ExpressionAttributeValues: {
+        ':uId': { S: userId },
+      },
+    },
+  );
+  return fetchResponse[0];
+};
+
+/**
  * This function processes the message that is sent to it by invoking the message handler function that is passed to it as a param
  * @param {object} AWS is the AWS sdk instance that needs to be passed from the handler
  * @param {string} region is the region of AWS that this service is running in
@@ -38,24 +78,30 @@ export const processMessage = async (AWS, region, service, account,
   );
 
   // eslint-disable-next-line no-undef-init
-  let serviceUserData = undefined;
+  let serviceAccountData = undefined;
   if (itemExists(msgBody, 'context')
     && itemExists(msgBody.context, 'user')
     && itemExists(msgBody.context.user, 'accountId')) {
-    const accData = await fetchRecordsByQuery(
-      AWS,
-      {
-        TableName: 'Account',
-        ExpressionAttributeNames: { '#pk': 'accountId' },
-        KeyConditionExpression: '#pk = :accId',
-        ExpressionAttributeValues: {
-          ':accId': { S: msgBody.context.user.accountId },
-        },
-      },
+    const accData = await getCurrentAccountData(
+      AWS, msgBody.context.user.accountId,
     );
     if (itemExists(accData, 'vendorData')
       && itemExists(accData.vendorData, `${service}`)) {
-      serviceUserData = accData.vendorData[`${service}`];
+      serviceAccountData = accData.vendorData[`${service}`];
+    }
+  }
+
+  // eslint-disable-next-line no-undef-init
+  let serviceUserData = undefined;
+  if (itemExists(msgBody, 'context')
+    && itemExists(msgBody.context, 'user')
+    && itemExists(msgBody.context.user, 'userId')) {
+    const userData = await getCurrentUserData(
+      AWS, msgBody.context.user.userId,
+    );
+    if (itemExists(userData, 'vendorData')
+      && itemExists(userData.vendorData, `${service}`)) {
+      serviceUserData = userData.vendorData[`${service}`];
     }
   }
 
@@ -65,28 +111,21 @@ export const processMessage = async (AWS, region, service, account,
       && isArray(dbConfig) && dbConfig.length > 0
       && typeof dbConfig[0].configdata !== 'undefined'
       ? dbConfig[0].configdata : [],
+    serviceAccountData,
     serviceUserData,
     attributes: msgAttribs,
   }, msgHandler);
   console.log(`processMessage: INFO: Result from worker is ${JSON.stringify(procRes, null, 4)}`);
 
-  if (itemExists(procRes.workerResp, 'serviceUserData')) {
-    if (typeof procRes.workerResp.serviceUserData !== 'object') {
-      throw new Error('Service specific user data should be an object');
+  if (itemExists(procRes.workerResp, 'serviceAccountData')) {
+    if (typeof procRes.workerResp.serviceAccountData !== 'object') {
+      throw new Error('Service specific user account data should be an object');
     }
     if (itemExists(msgBody, 'context')
       && itemExists(msgBody.context, 'user')
       && itemExists(msgBody.context.user, 'accountId')) {
-      const currAccData = await fetchRecordsByQuery(
-        AWS,
-        {
-          TableName: 'Account',
-          ExpressionAttributeNames: { '#pk': 'accountId' },
-          KeyConditionExpression: '#pk = :accId',
-          ExpressionAttributeValues: {
-            ':accId': { S: msgBody.context.user.accountId },
-          },
-        },
+      const currAccData = await getCurrentAccountData(
+        AWS, msgBody.context.user.accountId,
       );
       if (!itemExists(currAccData, 'vendorData')) {
         currAccData.vendorData = {};
@@ -96,9 +135,34 @@ export const processMessage = async (AWS, region, service, account,
       }
       currAccData.vendorData[`${service}`] = {
         ...currAccData.vendorData[`${service}`],
-        ...procRes.workerResp.serviceUserData,
+        ...procRes.workerResp.serviceAccountData,
       };
       await batchPutIntoDynamoDb(AWS, [currAccData], 'Account');
+    }
+  }
+
+
+  if (itemExists(procRes.workerResp, 'serviceUserData')) {
+    if (typeof procRes.workerResp.serviceUserData !== 'object') {
+      throw new Error('Service specific user data should be an object');
+    }
+    if (itemExists(msgBody, 'context')
+      && itemExists(msgBody.context, 'user')
+      && itemExists(msgBody.context.user, 'userId')) {
+      const currUserData = await getCurrentUserData(
+        AWS, msgBody.context.user.userId,
+      );
+      if (!itemExists(currUserData, 'vendorData')) {
+        currUserData.vendorData = {};
+      }
+      if (!itemExists(currUserData.vendorData, `${service}`)) {
+        currUserData.vendorData[`${service}`] = {};
+      }
+      currUserData.vendorData[`${service}`] = {
+        ...currUserData.vendorData[`${service}`],
+        ...procRes.workerResp.serviceUserData,
+      };
+      await batchPutIntoDynamoDb(AWS, [currUserData], 'User');
     }
   }
 
